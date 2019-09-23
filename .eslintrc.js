37d7d4ed74d19e30f2f5c1d41061af36a093b5df
Milco van Klingeren <mvklingeren@gmail.com>
2019-09-23 18:59:41 +0200

    sighn 400

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,14 @@ module.exports = {
   rules: {
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
-    "no-unused-vars": "off"
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "none",
+        ignoreRestSiblings: true
+      }
+    ]
   },
   settings: {
     react: {
