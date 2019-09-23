be1060553417f052f2139081d01b59c6cdd83163
Milco van Klingeren <mvklingeren@gmail.com>
2019-09-23 09:43:47 +0200

    SIGH

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,5 +1,7 @@
-// eslint-disable-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from "react";
+/* eslint-enable no-unused-vars */
+
 import { useRef, useState } from "react";
 import Project from "./project";
 /** @jsx jsx */ import { jsx, css } from "@emotion/core";
