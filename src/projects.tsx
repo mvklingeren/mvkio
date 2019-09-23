787790da81b921fb4b6c2af9cd2045565d9932b5
Milco van Klingeren <mvklingeren@gmail.com>
2019-09-23 11:21:13 +0200

    SIGH3

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useRef, useState } from "react";
 import Project from "./project";
